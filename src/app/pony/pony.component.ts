d9588ca0ca4fb88b57d403da888548da5a2a6563
Nicolas Hudavert <hudanico@hotmail.com>
2019-10-16 12:36:26 +0200

    pony component

---
+++ src/app/pony/pony.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { PonyModel } from '../models/pony.model';
+
+@Component({
+  selector: 'pr-pony',
+  templateUrl: './pony.component.html',
+  styleUrls: ['./pony.component.css']
+})
+export class PonyComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() ponyModel: PonyModel;
+  @Output() readonly ponyClicked = new EventEmitter<PonyModel>();
+
+  getPonyImageUrl() {
+    return 'assets/images/pony-' + this.ponyModel.color.toLowerCase() + '.gif';
+  }
+
+  ngOnInit() {
+  }
+  clicked() {
+    this.ponyClicked.emit(this.ponyModel);
+  }
+
+}
